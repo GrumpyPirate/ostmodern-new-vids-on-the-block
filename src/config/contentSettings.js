c52fdc8ba9db242de9c744e084b22123fcaabf06
Edward Cobbold <edward.cobbold@gmail.com>
2018-02-08 09:44:42 +0000

    Implement Set page

--- src/config/contentSettings.js
+++ src/config/contentSettings.js
@@ -1,3 +1,6 @@
+// API base
+const API_BASE_URL = 'http://feature-code-test.skylark-cms.qa.aws.ostmodern.co.uk:8000';
+
 // Set uids
 const sets = {
   home: 'coll_e8400ca3aebb4f70baf74a81aefd5a78'
@@ -5,5 +8,6 @@ const sets = {
 
 // Named exports
 export {
+  API_BASE_URL,
   sets
 };
